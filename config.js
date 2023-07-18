01fa09a9f726a85ff852f8f142012d8ca7023eea
levislait <130797634+levislait@users.noreply.github.com>
2023-07-18 17:25:08 +0300

    Update config.js

--- config.js
+++ config.js
@@ -7,12 +7,12 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['254765 777423', 'kinflux digital', true],
+  ['2250545752448', 'Levis 👌🐉', true],
   ['254110377776', 'zamzack254', true], 
   [''] 
 ] //Number of owners
 
-global.mods = ['254110077535','254110377776','254765777423'] 
+global.mods = ['254110077535','254110377776','254765777423','2250545752448'] 
 global.prems = ['254110077535', '254110377776', '2250545752448']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
@@ -47,7 +47,7 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = 'ₖᵢₙFₗᵤₓ Bₒₜ 🐉'
+global.botname = 'kinflux bot🐉'
 global.premium = 'true'
 global.packname = 'kinflux┃ᴮᴼᵀ' 
 global.author = 'zamzack254' 
